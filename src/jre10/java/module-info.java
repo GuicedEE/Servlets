23d2781dfd5b74c15c04e8617be7b7604280ad91
GedMarc <ged_marc@hotmail.com>
2018-11-01 01:10:38 +0200

    Transitive dependencies don't filter down, opening the requested module seems to work great

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -7,10 +7,14 @@ import com.jwebmp.guicedservlets.services.IGuiceSiteBinder;
 module com.jwebmp.guicedservlets {
 	uses IGuiceSiteBinder;
 
-	requires transitive com.google.guice.extensions.servlet;
-	requires transitive com.jwebmp.guicedinjection;
-
-	requires transitive javax.servlet.api;
+	requires com.google.guice.extensions.servlet;
+	requires com.jwebmp.guicedinjection;
+	requires com.jwebmp.logmaster;
+	requires com.google.guice;
+	requires java.logging;
+	requires javax.servlet.api;
+	requires aopalliance;
+	requires java.validation;
 
 	exports com.jwebmp.guicedservlets;
 	exports com.jwebmp.guicedservlets.services;
