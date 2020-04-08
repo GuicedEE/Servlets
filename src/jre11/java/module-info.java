6e67f1381e164824250f9845ed590f54d2eceb99
GedMarc <marc.magon@gmail.com>
2020-04-08 18:08:21 +0200

    Hibernate to 5.4.14

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -4,17 +4,14 @@ module com.guicedee.guicedservlets {
 
 	requires transitive com.google.guice.extensions.servlet;
 	requires transitive com.guicedee.guicedinjection;
-	requires transitive com.guicedee.logmaster;
-	requires transitive com.google.guice;
+	requires transitive javax.servlet.api;
 
 	requires java.logging;
-	requires javax.servlet.api;
-	requires aopalliance;
+
 	requires java.validation;
 
 	exports com.guicedee.guicedservlets;
 	exports com.guicedee.guicedservlets.services;
-
 
 	provides com.guicedee.guicedinjection.interfaces.IGuiceModule with com.guicedee.guicedservlets.services.GuiceSiteInjectorModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuicePreStartup with com.guicedee.guicedservlets.implementations.GuiceServletKeyStartup;
