f223d5f9f845612d2571044ce1e64bc378e94b9e
GedMarc <marc.magon@gmail.com>
2022-02-09 18:55:16 +0200

    Remove @Serial for jdk 11

--- src/main/java/com/guicedee/guicedservlets/CallScopeProperties.java
+++ src/main/java/com/guicedee/guicedservlets/CallScopeProperties.java
@@ -7,7 +7,7 @@ import java.io.*;
 @CallScope
 public class CallScopeProperties implements Serializable
 {
-	@Serial
+	//@Serial
 	private static final long serialVersionUID = 1L;
 	
 	private boolean webCall;
